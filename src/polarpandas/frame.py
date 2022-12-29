0a0a3ef6ed7e7ca6bf88ec23bd7a7d7c993c94e4
Odos Matthews <odosmatthews@Odoss-MacBook-Pro.local>
2022-12-29 12:25:43 -0500

    add __setitem__ method

--- src/polarpandas/frame.py
+++ src/polarpandas/frame.py
@@ -2,6 +2,10 @@
 A DataFrame object that behaves like a pandas DataFrame
 but using polars DataFrame to do all the work.
 """
+import polars as pl
 
-class DataFrame:
-    ...
+
+class DataFrame(pl.DataFrame):
+    def __setitem__(self, column: str, values) -> None:
+        ...
+        #self.with_columns([(values).alias(column)])
